ad5627043bff3dd74e68ac02107779fb1455ed68
Andres Santamarina <andresesantamarina@gmail.com>
2023-10-25 17:14:12 -0300

    ejercicio 6 del practico 1 realizado

--- js/practicoEjercicio6.js
+++ js/practicoEjercicio6.js
@@ -7,3 +7,15 @@ input: 15 , 3
 
 Output: El 15 es el número más grande
 */
+let numero1 = parseInt(prompt("Ingrese un número"));
+let numero2 = parseInt(prompt("Ingrese otro número"));
+if (numero1 > numero2) {
+    document.write(`El número mayor es el ${numero1}`)
+}
+else if (numero1 != numero2){
+    document.write(`El número mayor es el ${numero2}`)
+}
+else{
+    document.write("Los números son iguales")
+};
+
