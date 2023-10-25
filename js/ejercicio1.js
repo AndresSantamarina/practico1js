34009735a6ddccf5f79cf261c4ba288a5035a7cc
Andres Santamarina <andresesantamarina@gmail.com>
2023-10-25 00:55:49 -0300

    ejercicio 1 de Trello realizado

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -8,7 +8,19 @@ Resta de numero1 y numero2.
 Multiplicación de numero1 y numero2.
 División de numero1 entre numero2.
 */
+let numero1 = 7;
+let numero2 = 2;
 
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+console.log("Los números usados son " + numero1 + " y " + numero2)
+console.log("Resultado de la suma: " + suma)
+console.log("Resultado de la resta: " + resta)
+console.log("Resultado de la multiplicación: " + multiplicacion)
+console.log("Resultado de la división: " + division)
 
 
 
