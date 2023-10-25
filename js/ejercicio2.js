0b7e55c4466e84db68bf4198bff85b59b546d091
Andres Santamarina <andresesantamarina@gmail.com>
2023-10-25 00:59:30 -0300

    ejercicio 2 de Trello realizado

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -3,3 +3,11 @@ Ejercicio 2: Concatenación de Cadenas
 
 Crea dos variables, nombre y apellido, y asígnales valores de tu elección. Luego, crea una tercera variable llamada nombreCompleto que contenga la concatenación de nombre y apellido. Muestra nombreCompleto en la consola.
 */
+
+let nombre = "Andrés";
+let apellido = "Santamarina";
+let nombreCompleto = nombre + " " + apellido;
+
+console.log("Nombre completo: " + nombreCompleto)
+
+
