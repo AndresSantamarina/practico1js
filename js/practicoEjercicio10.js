6b51050efbd41ae1bf44072de050e10370a1f399
Andres Santamarina <andresesantamarina@gmail.com>
2023-10-25 18:13:07 -0300

    ejercicio 10 del práctico 1 realizado

--- js/practicoEjercicio10.js
+++ js/practicoEjercicio10.js
@@ -3,7 +3,22 @@
 Ejemplo: 
 
 input: 20
-
-
 Output: El 20 es divisible por 2.
 */
+let numero = parseInt(prompt("Ingrese un número"));
+
+if (numero % 2 == 0 && numero != 0) {
+    document.write(`El número ${numero} es divisible por 2`)
+}
+else if (numero % 3 == 0 && numero != 0) {
+    document.write(`El número ${numero} es divisible por 3`)
+}
+else if (numero % 5 == 0 && numero != 0) {
+    document.write(`El número ${numero} es divisible por 5`)
+}
+else if (numero % 7 == 0 && numero != 0) {
+    document.write(`El número ${numero} es divisible por 7`)
+}
+else {
+    document.write(`El número ${numero} no es divisible por 2, 3 ,5 o 7`)
+}
