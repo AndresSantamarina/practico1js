f202f69010a5f1e80c2b059a5dd4eff5b6f470f1
Andres Santamarina <andresesantamarina@gmail.com>
2023-10-25 18:02:47 -0300

    ejercicio 9 del práctico 1 realizado

--- js/practicoEjercicio9.js
+++ js/practicoEjercicio9.js
@@ -6,3 +6,15 @@ Ejemplo:
 input: Hola mundo
 Output: oauo
 */
+let frase = prompt("Ingrese una frase");
+let vocales = "";
+
+for (let letra = 0; letra < frase.length; letra++) {
+    let caracter = frase.charAt(letra).toLowerCase();
+
+    if (caracter === "a" || caracter === "e" || caracter === "i" || caracter === "o" || caracter === "u" || caracter === "á" || caracter === "é" || caracter === "í" || caracter === "ó" || caracter === "ú") {
+        vocales = vocales + caracter;
+    }
+}
+
+document.write(`Las vocales de la frase ingresada son: ${vocales}`);
