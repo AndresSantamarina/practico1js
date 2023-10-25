44000ba4ea70ffcbcb48b71c6a978c89e7776444
Andres Santamarina <andresesantamarina@gmail.com>
2023-10-25 01:20:50 -0300

    ejercicio 5 del practico 1 realizado

--- js/practicoEjercicio5.js
+++ js/practicoEjercicio5.js
@@ -3,3 +3,6 @@
 Nota: Tener en cuenta la siguiente función: parseInt
 https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/parseInt
 */
+let numero1 = parseInt(prompt("Ingrese un número"));
+let numero2 = parseInt(prompt("Ingrese otro número"));
+let resultado = document.write("El resultado de la suma es " + (numero1+numero2));
